da248b48eba3334dd344023f3d7921757752c716
kabu1 <bensonmk99@gmail.com>
2022-01-23 12:43:18 +0300

    refine parser service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { MarkdownParserService } from './markdown-parser.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [ MarkdownParserService]
 })
 export class AppComponent {
-  title = 'mackdown';
+  convertedtext = 'string';
+  constructor(
+    private md: MarkdownParserService){
+  }
+  updateOutput(mdText:string){
+    this.convertedtext = this.md.convert(mdText);
+  }
 }
--- src/app/markdown-parser.service.ts
+++ src/app/markdown-parser.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
-import { marked } from 'marked';
+import * as marked  from 'marked';
 @Injectable({
   providedIn: 'root'
 })
 export class MarkdownParserService {
 
-  constructor() { }
+private md: any;
+
+  constructor() {
+
+    this.md = marked;
+    this.md.setOptions({
+      // renderer: new marked.Renderer(),
+      // highlight: function(code: any, lang: any) {
+      //   const hljs = require('highlight.js');
+      //   const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+      //   return hljs.highlight(code, { language }).value;
+      // },
+      // langPrefix: 'hljs language-', // highlight.js css expects a top-level 'hljs' class.
+      pedantic: false,
+      gfm: true,
+      breaks: false,
+      sanitize: false,
+      smartLists: true,
+      smartypants: false,
+      xhtml: false
+    });
+
+
+   }
+   convert(markdown:string){
+     return this.md.parse(markdown);
+   }
 }
